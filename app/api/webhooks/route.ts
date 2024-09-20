a05afbbb3195c40fb277ec275483630b84dea229
AAArmeNNN <armen.etarian@gmail.com>
2024-09-20 18:19:26 +0200

    hello

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -21,19 +21,29 @@ const relevantEvents = new Set([
   'customer.subscription.deleted'
 ]);
 console.log("Hello API/route")
+
+
 export async function POST(req: Request) {
   const body = await req.text();
   const sig = req.headers.get('stripe-signature') as string;
+  console.log(sig)
+
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  console.log(webhookSecret)
+
   let event: Stripe.Event;
 
   try {
     if (!sig || !webhookSecret)
       return new Response('Webhook secret not found.', { status: 400 });
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    console.log("Webhook received")
+
     console.log(`🔔  Webhook received: ${event.type}`);
   } catch (err: any) {
     console.log(`❌ Error message: ${err.message}`);
+    console.log("Error")
+
     return new Response(`Webhook Error: ${err.message}`, { status: 400 });
   }
 
