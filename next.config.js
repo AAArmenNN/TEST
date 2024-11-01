0da282eb25327fa3bd0d1bd1cb4949570f066cf7
AAArmeNNN <armen.etarian@gmail.com>
2024-11-01 15:58:25 +0100

    bug?

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-
-
-module.exports = {
-  reactStrictMode: false, // Désactive React Strict Mode pour éviter les rendus multiples
-  devIndicators: {
-    autoPrerender: false, // Désactive le pré-rendu automatique pour éviter Fast Refresh
-  },
-};
